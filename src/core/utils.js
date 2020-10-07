0f3710b109a2e9b346e0067562d5e7f6d46830ec
Aventusa <Aventusa@yandex.ru>
2020-10-07 16:52:55 +0300

    Finish base table logic

--- src/core/utils.js
+++ src/core/utils.js
@@ -5,3 +5,14 @@ export function copitalize(string) {
 
     return string.charAt(0).toUpperCase() + string.slice(1)
 }
+
+export function range(start, end) {
+    if (start > end) {
+        [end, start] = [start, end]
+    }
+    return new Array(end - start + 1)
+        .fill('')
+        .map((_, index) => {
+            return start + index
+        })
+}
